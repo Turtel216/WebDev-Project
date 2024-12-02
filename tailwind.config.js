bdc66d7381ee90a08881ee6aeed7293dd163b1a6
Turtel216 <papadimitrios216@gmail.com>
2024-12-02 11:44:05 +0200

    removed example

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        taki: 'taki 2s infinite',
+      },
+      keyframes: {
+        taki: {
+          '0%, 100%': {
+            transform: 'rotate(0deg) scale(1)',
+            backgroundColor: '#FF69b4'
+          },
+          '50%': {
+            transform: 'rotate(-15deg) scale(0.9)',
+            backgroundColor: '#FF1439'
+          },
+          '75%': {
+            transform: 'rotate(-15deg) scale(1.05)',
+            backgroundColor: '#FFc0cb'
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
